0c6d120250f4c2da582784217319ad5fac5a7199
luismurri <luismurri@unam.edu>
2026-03-16 01:48:59 +0000

    tarea terminada

---
+++ Clase 1/Programa_1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    // Imprime el mensaje en la terminal
+    printf("¡Hola mundo!\n");
+    
+    return 0;
+}
---
+++ Clase 1/programa2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    float a,b,c;
+    printf("ingresa el primer numero\n");
+    scanf("%f",&a);
+    printf("ingresa el segundo numero\n");
+    scanf("%f",&b);
+    c=a+b;
+    printf("el resultado de sumar %f + %f = %f\n", a, b, c);
+return 0;
+}
